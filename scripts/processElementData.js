650978b25f68979695590c2f273844edd421bac6
Simei Thander <simeithander@gmail.com>
2020-11-02 18:51:16 -0300

    Upload inicial

---
+++ scripts/processElementData.js
@@ -0,0 +1,29 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_xbkMwR1FEeuJ7tBHwqAA5g,_7y-sgR1FEeuJ7tBHwqAA5gActivity"]="./../../Catálogo de Serviços/deliveryprocesses/resources/Atribuir incidente_D835CD8B_3c5ada7e_Activity.jpeg"
+contentPage.processPage.imageFiles["_xbkMwR1FEeuJ7tBHwqAA5gActivity"]="./../../Catálogo de Serviços/deliveryprocesses/resources/analise_B971AB9F_b971ab9f_Activity.jpeg"
+contentPage.processPage.elementUrls["_7y-sgR1FEeuJ7tBHwqAA5g"] = ["Incidente","Catálogo de Serviços/deliveryprocesses/Atribuir incidente_D835CD8B.html"];
+contentPage.processPage.elementUrls["_GPMMuh1GEeuJ7tBHwqAA5g"] = ["Coordenador","Catálogo de Serviços/deliveryprocesses/coordenador_B1714B15.html"];
+contentPage.processPage.elementUrls["_GPMzxx1GEeuJ7tBHwqAA5g"] = ["Solução do incidente","Catálogo de Serviços/deliveryprocesses/solucao_do_incidente_30B8006F.html"];
+contentPage.processPage.elementUrls["_GPMzxR1GEeuJ7tBHwqAA5g"] = ["Transferir incidente","Catálogo de Serviços/deliveryprocesses/transferir_incidente_37810C49.html"];
+contentPage.processPage.elementUrls["_GPMMsh1GEeuJ7tBHwqAA5g"] = ["Resolução de incidente","Catálogo de Serviços/deliveryprocesses/resolucao_de_incidente_284D86D7.html"];
+contentPage.processPage.elementUrls["_GPMMuB1GEeuJ7tBHwqAA5g"] = ["Cliente","Catálogo de Serviços/deliveryprocesses/cliente_B83A56EF.html"];
+contentPage.processPage.elementUrls["_GPMMsR1GEeuJ7tBHwqAA5g"] = ["Atualizar incidente","Catálogo de Serviços/deliveryprocesses/atualizar_incidente_39B472C1.html"];
+contentPage.processPage.elementUrls["_GPMMth1GEeuJ7tBHwqAA5g"] = ["Técnico","Catálogo de Serviços/deliveryprocesses/tecnico_ECDF68F6.html"];
+contentPage.processPage.elementUrls["_xbkMwR1FEeuJ7tBHwqAA5g"] = ["Análise","Catálogo de Serviços/deliveryprocesses/analise_B971AB9F.html"];
+contentPage.processPage.elementUrls["_GPMMtB1GEeuJ7tBHwqAA5g"] = ["Chamado","Catálogo de Serviços/deliveryprocesses/chamado_F3A874D0.html"];
+contentPage.processPage.elementUrls["_GPMzwx1GEeuJ7tBHwqAA5g"] = ["Status do incidente","Catálogo de Serviços/deliveryprocesses/status_do_incidente_6C261E50.html"];
+contentPage.processPage.elementUrls["_GPMMsx1GEeuJ7tBHwqAA5g"] = ["Transferir incidente","Catálogo de Serviços/deliveryprocesses/transferir_incidente_32EB66E7.html"];
+contentPage.processPage.elementUrls["_GPMMsB1GEeuJ7tBHwqAA5g"] = ["Atribuir incidente","Catálogo de Serviços/deliveryprocesses/atribuir_incidente_2F1692B1.html"];
+contentPage.processPage.elementUrls["_GPMzwR1GEeuJ7tBHwqAA5g"] = ["Analise de incidente","Catálogo de Serviços/deliveryprocesses/analise_de_incidente_72EF2A2A.html"];
